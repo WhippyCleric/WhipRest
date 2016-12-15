b7ef41c4f026bcf160218b0872a7ffb98fa93dcc
DUNN Matthewstuart <Matthewstuart.DUNN@murex.com>
2016-12-15 09:25:27 +0100

    Add all HTTP METHODS support, allow for long paths, pass body to callbacks, parse params and pass to callbacks

--- src/WhipRest.jl
+++ src/WhipRest.jl
@@ -4,15 +4,108 @@ using HttpServer
 
 include("beans.jl")
 
-  export ServerConfig
+  export ServerConfig, ResponseConfig, WhipRestServer, GET, POST, createServer, startServer
 
 
-  type ServerConfig
-    x
+  function createServer(config::ServerConfig)
+    restServer = WhipRestServer(config.port, Dict{String,Any}(),  Dict{String,Any}(), Dict{String,Any}(), Dict{String,Any}(), Dict{String,Any}())
+    #Iterate over all the request response mappings
+    for i = 1:length(config.responseConfigurations)
+        responseConfiguration = config.responseConfigurations[i]
+        #get the correct mappings set based on HTTP method
+        mappings = getMappings(restServer, responseConfiguration.method)
+        #Put the response handler into map for the given path
+        mappings[responseConfiguration.path] = responseConfiguration.responseHandler
+    end
+    return restServer
   end
 
-#  function hello(message::String)
-#    print(message)
-#  end
+  function startServer(whipServer::WhipRestServer)
+    #Create a new HTTP handler
+    http = HttpHandler() do req::Request, res::Response
+      #Get the mappings of path to action based on HTTP method
+      mappings = getMappings(whipServer, getHTTP_METHOD(req.method))
+      try
+        #Extract the path from the request resource
+        path = getPath(req.resource)
+        #Try to find the function for the mapping
+        responseHandler = mappings[String(path)]
+        #Call the handler for this path with the content after the path
+        params = getParams(req.resource)
+        Response(responseHandler(params, ASCIIString(req.data)))
+      catch error
+        if isa(error, KeyError)
+           #No mapping was found for this path
+           Response("404")
+        end
+      end
+
+    end
+    server = Server(http)
+    run(server, whipServer.port)
+  end
+
+  function getPath(resource::String)
+    #Get the entire resource from the first / to the ? or the end of string
+    paramIndex = search(resource, '?')
+    if paramIndex==0
+      #Trim the / off the end if it exists
+      if resource[end] == '/'
+        path = resource[1:end-1]
+      else
+        path = resource[1:end]
+      end
+    else
+      path = resource[1:paramIndex-1]
+    end
+    return path
+  end
+
+  function getParams(resource::String)
+    paramIndex = search(resource, '?')
+    paramsMap = Dict{String,String}()
+    if paramIndex==0
+      return paramsMap
+    else
+      params = split(resource[paramIndex+1:end], '&')
+      for i = 1:length(params)
+        pair = split(params[i], '=')
+        paramsMap[pair[1]] = String(pair[2])
+      end
+
+      return paramsMap
+    end
+  end
+
+
+  function getHTTP_METHOD(method::String)
+    if method == "GET"
+      return GET
+    elseif method== "POST"
+      return POST
+    elseif method == "PUT"
+      return PUT
+    elseif method == "PATCH"
+      return PATCH
+    elseif method == "DELETE"
+      return DELETE
+    end
+  end
+
+
+
+  function getMappings(whipServer::WhipRestServer, method::HTTP_METHOD)
+    if GET == method
+      return whipServer.getMappings
+    elseif POST == method
+      return whipServer.postMappings
+    elseif PUT == method
+      return whipServer.putMappings
+    elseif PACTH == method
+      return whipServer.patchMappings
+    elseif DELETE == method
+      return whipServer.deleteMappings
+    end
+  end
 
 end #end module WhipRest
--- src/beans.jl
+++ src/beans.jl
@@ -1,3 +1,21 @@
 type ServerConfig
+  port
+  responseConfigurations
+end
+
+@enum HTTP_METHOD GET POST PUT PATCH DELETE
+
+type ResponseConfig
+  method::HTTP_METHOD
   path::String
+  responseHandler
+end
+
+type WhipRestServer
+  port
+  getMappings::Dict{String,Any}
+  postMappings::Dict{String,Any}
+  putMappings::Dict{String,Any}
+  patchMappings::Dict{String,Any}
+  deleteMappings::Dict{String,Any}
 end
